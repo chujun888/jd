589273571038ea7def0e2ea8a2e0b1dba1c66c8c
unknown <626905561@qq.com>
2018-02-17 11:06:18 +0800

    17-2

---
+++ index.js
@@ -0,0 +1,18 @@
+window.onload=function(){
+	
+}
+var search=function(){
+	window.onscroll=function(){
+		var header=document.getElementsByTagName("header")[0];
+		var banner=document.getElementsByClassName("banner");
+
+		var top=document.body.scrollTop;
+		var height=banner.offsetHeight;
+		if(top>height){
+			
+		}
+		else{
+			
+		}
+	}
+}
